751c2b34a40393cfd12295853db514f5acd35b5a
MinimalMio <yukati2024@outlook.com>
2024-12-21 11:59:44 +0000

    [feat/wip] config system

--- src/config.h
+++ src/config.h
@@ -1,6 +1,16 @@
 #ifndef __MIOCHAT_CONFIG
 #define __MIOCHAT_CONFIG
 
-extern void config_read();
+typedef struct {
+  int server_port;
+  char welcome_message[1024];
+  char db_file[256];
+} Config;
+
+#define MAX_LINE_LENGTH 256
+
+extern void trim_whitespace(char *str);
+
+extern int parse_config(const char *filename, Config *config);
 
 #endif // __MIOCHAT_CONFIG
